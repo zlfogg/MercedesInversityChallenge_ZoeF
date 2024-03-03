45132f08abc26adbaa825ee94eba0cd9a39ada6e
zlfogg <161863503+zlfogg@users.noreply.github.com>
2024-03-03 19:32:16 +0000

    laptime js

--- website.js
+++ website.js
@@ -20,3 +20,49 @@ function setLapsRemaining() {
     alert('Please enter a valid number of laps (above 0).');
   }
 }
+
+let laptime = 0;
+
+//function setLapTime() {
+  //const optionSoft = document.getElementById('option1');
+  //const optionMed = document.getElementById('option2');
+  //const optionHard = document.getElementById('option3');
+
+  //if (optionSoft.checked) {
+    //laptime = 90;
+    //alert(`Lap time set to: ${laptime}`);
+  //} else if (optionMed.checked) {
+    //laptime = 92;
+    //alert(`set to: ${laptime}`);
+  //} else if (optionHard.checked) {
+    //laptime = 95;
+    //alert(`set to ${laptime}`);
+  //}
+//}
+//
+
+const optionSoft = document.getElementById('option1');
+const optionMed = document.getElementById('option2');
+const optionHard = document.getElementById('option3');
+
+optionSoft.addEventListener('change', function() {
+    if (this.checked) {
+      setLapTime(90);
+    }
+  });
+  
+optionMed.addEventListener('change', function() {
+    if (this.checked) {
+      setLapTime(92);
+    }
+  });
+
+optionHard.addEventListener('change', function() {
+    if (this.checked) {
+      setLapTime(95);
+    }
+  });
+
+function setLapTime(laptime) {
+    alert(`Lap time set to: ${laptime}`);
+}

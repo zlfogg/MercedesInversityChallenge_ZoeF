c1e32378628d20ead9514b0022aa799a57e41337
zlfogg <161863503+zlfogg@users.noreply.github.com>
2024-03-03 19:06:14 +0000

    laps remaining box

--- website.js
+++ website.js
@@ -8,3 +8,15 @@ function getSelectedOption() {
       }
     });
   }
+
+  let lapsRemaining = 0;
+
+function setLapsRemaining() {
+  const lapsInput = document.getElementById('lapsInput').value;
+  
+  if (lapsInput !== '' && parseInt(lapsInput) > 0) {
+    lapsRemaining = parseInt(lapsInput);
+  } else {
+    alert('Please enter a valid number of laps (above 0).');
+  }
+}
